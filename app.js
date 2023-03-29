d560e8a5915b1b2cc7db1f048be770fca39c5a5b
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-03-28 21:41:13 -0400

    created object that mailchimp can understand

--- app.js
+++ app.js
@@ -3,20 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended: true}))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
 
-app.get("/", (req,res) => {
-  res.sendFile(__dirname + "/signup.html")
-})
+app.post("/", (req, res) => {
+  const fName = req.body.firstName;
+  const lName = req.body.lastName;
+  const email = req.body.email;
 
-app.post("/", (req,res) => {
-  var fName = req.body.firstName
-  var lName = req.body.lastName
-  var email = req.body.email
-})
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  var jsonData = JSON.stringify(data);
+
+  
+});
 
 app.listen(4000, () => {
   console.log("server is up and running on port 4000");
 });
+
+// my API Key
+// 41b8e8396b51051582fbd6e1d0aaa0f3-us21
+
+//audience id
+// 9f7bf0a398
